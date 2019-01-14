406b990d016ea34f1ea0aaac372834d325971a85
aydansmith <43067348+aydansmith@users.noreply.github.com>
2019-01-13 19:17:10 -0600

    Update OI.java

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -6,17 +6,6 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
-
-/**
- * This class is the glue that binds the controls on the physical operator
- * interface to the commands and command groups that allow control of the robot.
- */
-
- public class OI {
-  package frc.robot;
-import edu.wpi.first.wpilibj.XboxController;
-
-
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
